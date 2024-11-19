702cd690c9e8db3770a0bd956b96537d094d8816
SULAIMAN KHAN <S3028837@live.tees.ac.uk>
2024-11-19 14:31:14 +0000

    create Staff class

---
+++ ThAmCo.Events/Data/Staff.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ThAmCo.Events.Data
+{
+    public class Staff
+    {
+        [Required]
+        public int staffId { get; set; }
+        public string staffName { get; set; }
+        [EmailAddress]
+        public string staffEmail { get; set; }
+        public bool firstAidTrained { get; set; }
+    }
+}
