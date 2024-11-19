aaf8dde4f9b62a7b7a549d6a6dfbe7763ac2a4d5
SULAIMAN KHAN <S3028837@live.tees.ac.uk>
2024-11-19 14:20:52 +0000

    create Event class

---
+++ ThAmCo.Events/Data/Event.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace ThAmCo.Events.Data
+{
+    public class Event
+    {
+        [Required]
+        public int eventId {  get; set; }
+        [MaxLength(100)]
+        public string eventName { get; set; }
+        public string eventType { get; set; }
+        public DateOnly? eventDate {  get; set; }
+    }
+}
