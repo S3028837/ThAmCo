d03c58cdcbf97ec19e976d9661bb6d9bd9bd17f4
S3028837 <S3028837@TU119007>
2024-11-21 12:47:47 +0000

    scaffold EventList razor page

---
+++ ThAmCo.Events/Pages/EventList/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Pages.EventList
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ThAmCo.Events.Data.EventsDbContext _context;
+
+        public DeleteModel(ThAmCo.Events.Data.EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Event Event { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var event = await _context.Events.FirstOrDefaultAsync(m => m.EventId == id);
+
+            if (event == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Event = event;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var event = await _context.Events.FindAsync(id);
+            if (event != null)
+            {
+                Event = event;
+                _context.Events.Remove(Event);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ThAmCo.Events/Pages/EventList/Details.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Pages.EventList
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ThAmCo.Events.Data.EventsDbContext _context;
+
+        public DetailsModel(ThAmCo.Events.Data.EventsDbContext context)
+        {
+            _context = context;
+        }
+
+        public Event Event { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var event = await _context.Events.FirstOrDefaultAsync(m => m.EventId == id);
+            if (event == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Event = event;
+            }
+            return Page();
+        }
+    }
+}
