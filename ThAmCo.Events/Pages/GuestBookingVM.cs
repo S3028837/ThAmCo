f13d213560056648578437717eec78b15082dfd4
SULAIMAN KHAN <S3028837@live.tees.ac.uk>
2024-12-10 13:54:39 +0000

    update guest details page to show information about events a guest is booked onto

---
+++ ThAmCo.Events/Pages/GuestBookingVM.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ThAmCo.Events.Data;
+
+namespace ThAmCo.Events.Pages
+{
+    public class GuestBookingVM
+    {
+        public int GuestBookingId { get; set; }
+        public int EventId {  get; set; }       
+        public int GuestId {  get; set; }
+        public bool Attendance {  get; set; }
+        public string EventName {  get; set; }
+        public string GuestName { get; set; }
+        [EmailAddress]
+        public string? GuestEmail {  get; set; }
+        [Phone]
+        public string? GuestPhone {  get; set; }
+    }
+}
--- ThAmCo.Events/Pages/GuestList/Details.cshtml.cs
+++ ThAmCo.Events/Pages/GuestList/Details.cshtml.cs
@@ -18,7 +18,12 @@ namespace ThAmCo.Events.Pages.GuestList
             _context = context;
         }
 
+        //gets a guests details
         public Guest Guest { get; set; } = default!;
+
+
+        //gets a list of guest bookings
+        public IList<GuestBooking> GuestBooking { get; set; } = default!;
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -27,6 +32,7 @@ namespace ThAmCo.Events.Pages.GuestList
                 return NotFound();
             }
 
+            //getsa specific guests details
             var guest = await _context.Guests.FirstOrDefaultAsync(m => m.GuestId == id);
             if (guest == null)
             {
@@ -36,7 +42,21 @@ namespace ThAmCo.Events.Pages.GuestList
             {
                 Guest = guest;
             }
+
+            //gets the guest bookings that the guest has booked onto
+            var bookingContext = _context.GuestBookings.AsQueryable();
+            if (id != null)
+            {
+                bookingContext = bookingContext.Where(b => b.GuestId == id);
+            }
+            bookingContext = bookingContext
+                .Include(g => g.Guest)
+                .Include(g => g.Event);
+
+            GuestBooking = await bookingContext.ToListAsync();
+
             return Page();
         }
+
     }
 }
