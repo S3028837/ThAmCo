64c4549d4964338f71d302fa04b79f9b1f9c0b1c
SULAIMAN KHAN <S3028837@live.tees.ac.uk>
2024-12-12 12:20:45 +0000

    implement FoodBookingDto so a booking can be made for an event

---
+++ ThAmCo.Catering/Dto/FoodBookingDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ThAmCo.Catering.Dto
+{
+    public class FoodBookingDto
+    {
+        public int FoodBookingId {  get; set; }
+        public int EventId { get; set; }
+        public int NumOfGuests { get; set; }
+        public int MenuId { get; set; }
+    }
+}
